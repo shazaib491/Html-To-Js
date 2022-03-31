cf65f732909c9709dcfbb56da1773347f3dc853e
shazaib491 <shazaibrehman127@gmail.com>
2022-04-01 01:21:27 +0530

    latest

--- index.js
+++ index.js
@@ -1,20 +1,132 @@
-console.log("Listening at browser")
-
-console.log(document)
-
-
-//
-let p=document.getElementById("auto"); 
-p.innerHTML+="Ashish"
 
 
 
-let cls=document.getElementsByClassName('demo');
-cls[0].innerHTML="Ashsihs bhai jindabad"
+// console.log(no)
+// // prashant 10 
+// // error
+// // not defined
 
-// npm node  package manager
+
+// var no=10;
+// console.log(no);
 
 
-function save(){
-    console.log("Hello Ashishh")
+
+
+
+// // dynamic type / static programing
+// // var varname=value
+// // printf();
+
+
+// console.log("Hello World")
+// console.error("Error Occured")
+// console.warn("Warning ")
+// console.info("This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or  someone's account, it is a scam and will give them access to your Facebook account")
+// console.info("This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or  someone's account, it is a scam and will give them access to your Facebook account")
+// console.clear()
+// console.table("asset","asset")
+// var name="admin";
+
+// // pritive       non primitive
+//     // number       array
+//                     // object
+//                     // null
+//                     // symbol
+//     // string
+//     // boolean
+//     // undefined
+
+// var no=10.0;
+// console.log(typeof no)
+// var name="string";
+// console.log(name)
+// var no;
+
+// // hoisting 
+// problem 1
+var name="string"
+
+console.log(no)
+var no=10
+console.log(name)
+
+// let const 
+// redefined
+var name="admin"
+
+
+
+// 
+var name="saman"
+
+console.log(name)
+
+// overwritge
+var name2="admin"
+
+name2="aman"
+
+console.log(name2)
+
+
+// 4
+
+// scoping 4
+// 
+{
+    var no=10;
+}
+
+console.log(no)
+
+// for(var i=0; i<=5; i++){
+//     console.log(i)
+//     // 0
+//     // 1
+//     // 2
+//     // 3
+//     // 4
+//     // 5
+//     // 6
+// }
+
+// console.log(i)
+
+
+// function name(){
+//     var elem=10;
+// }
+
+// console.log(elem);
+
+let sname="prashant";
+console.log(sname);
+// redefined
+
+// overwrite
+sname="aman"
+console.log(sname)
+
+// scoping
+{
+    let sno=50;
+}
+
+// console.log(sno)
+
+
+for(let i=0; i<=5; i++){
+    console.log(i);
+}
+
+// console.log(i);
+
+
+const username="Admin";
+// username="admin"
+
+{
+    const  sum=10+20;
+    console.log(sum)
 }

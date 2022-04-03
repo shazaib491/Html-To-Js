ebd419029a8f03fa1d3e3323ad8e7038d127b9c4
shazaib491 <shazaibrehman127@gmail.com>
2022-04-04 01:57:36 +0530

    latest

--- index.js
+++ index.js
@@ -1,132 +1,266 @@
 
 
 
+// // console.log(no)
+// // // prashant 10 
+// // // error
+// // // not defined
+
+
+// // var no=10;
+// // console.log(no);
+
+
+
+
+
+// // // dynamic type / static programing
+// // // var varname=value
+// // // printf();
+
+
+// // console.log("Hello World")
+// // console.error("Error Occured")
+// // console.warn("Warning ")
+// // console.info("This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or  someone's account, it is a scam and will give them access to your Facebook account")
+// // console.info("This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or  someone's account, it is a scam and will give them access to your Facebook account")
+// // console.clear()
+// // console.table("asset","asset")
+// // var name="admin";
+
+// // // pritive       non primitive
+// //     // number       array
+// //                     // object
+// //                     // null
+// //                     // symbol
+// //     // string
+// //     // boolean
+// //     // undefined
+
+// // var no=10.0;
+// // console.log(typeof no)
+// // var name="string";
+// // console.log(name)
+// // var no;
+
+// // // hoisting 
+// // problem 1
+// var name="string"
+
 // console.log(no)
-// // prashant 10 
-// // error
-// // not defined
-
-
-// var no=10;
-// console.log(no);
-
-
-
-
-
-// // dynamic type / static programing
-// // var varname=value
-// // printf();
-
-
-// console.log("Hello World")
-// console.error("Error Occured")
-// console.warn("Warning ")
-// console.info("This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or  someone's account, it is a scam and will give them access to your Facebook account")
-// console.info("This is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or  someone's account, it is a scam and will give them access to your Facebook account")
-// console.clear()
-// console.table("asset","asset")
-// var name="admin";
-
-// // pritive       non primitive
-//     // number       array
-//                     // object
-//                     // null
-//                     // symbol
-//     // string
-//     // boolean
-//     // undefined
-
-// var no=10.0;
-// console.log(typeof no)
-// var name="string";
+// var no=10
 // console.log(name)
-// var no;
 
-// // hoisting 
-// problem 1
-var name="string"
-
-console.log(no)
-var no=10
-console.log(name)
-
-// let const 
-// redefined
-var name="admin"
+// // let const 
+// // redefined
+// var name="admin"
 
 
 
-// 
-var name="saman"
+// // 
+// var name="saman"
 
-console.log(name)
+// console.log(name)
 
-// overwritge
-var name2="admin"
+// // overwritge
+// var name2="admin"
 
-name2="aman"
+// name2="aman"
 
-console.log(name2)
+// console.log(name2)
 
 
-// 4
+// // 4
 
-// scoping 4
-// 
-{
-    var no=10;
-}
+// // scoping 4
+// // 
+// {
+//     var no=10;
+// }
 
-console.log(no)
+// console.log(no)
 
-// for(var i=0; i<=5; i++){
+// // for(var i=0; i<=5; i++){
+// //     console.log(i)
+// //     // 0
+// //     // 1
+// //     // 2
+// //     // 3
+// //     // 4
+// //     // 5
+// //     // 6
+// // }
+
+// // console.log(i)
+
+
+// // function name(){
+// //     var elem=10;
+// // }
+
+// // console.log(elem);
+
+// let sname="prashant";
+// console.log(sname);
+// // redefined
+
+// // overwrite
+// sname="aman"
+// console.log(sname)
+
+// // scoping
+// {
+//     let sno=50;
+// }
+
+// // console.log(sno)
+
+
+// for(let i=0; i<=5; i++){
+//     console.log(i);
+// }
+
+// // console.log(i);
+
+
+// const username="Admin";
+// // username="admin"
+
+// {
+//     const  sum=10+20;
+//     console.log(sum)
+// }
+
+
+// console.log(5**2)
+let a=10;
+// let b=a;
+// b+=20;
+
+
+
+// // let name="Rahul"
+// // let lname="Gupta"
+// // let username=name+'-'+lname;
+// // console.log(username)
+// // let fname=+'10sdff';
+
+// // console.log(fname)
+// // let no=20;
+
+// // console.log(fname+no);
+
+// // your first name is prashant and second name is kumar and your subject mca and semester is 3rd
+// let fname="Prashant"
+// let lname="Kumar"
+// let cls="MCA"
+// let sem="3rd"
+
+// // let userDetail="your first name is \n"+fname+" "+"Second name is \n"+lname+" "+"subject is \n"+cls+" "+"Semester is"+sem;
+// // // es6
+// // console.log(userDetail)
+// // // template literal
+// // let userDetailCopy=`your first name is ${fname} Second name is ${lname}  subject is ${cls}  Semester is ${sem}
+// // `
+
+// // console.log(userDetailCopy)
+
+// // let s="5" * "5";
+// // console.log(s);
+
+// // console.log(10=='10') // true
+// // console.log(10==='10') // true
+// // console.log(10!=20)
+// // console.log(10!=='10')
+
+// console.log((20 > 10 || 10 > 50)?'10 is greater':'10 is lesser')
+// // and operator
+// // 1 0 0
+// // 1 1 1
+// // 0 0 0
+// // 0 1 0
+
+// // or operator
+// // 1 0 1
+// // 1 1 1
+// // 0 0 0
+// // 0 1 1
+
+// // let no=10;
+// // console.log(typeof no)
+
+// // while loop => 
+// // do while loop
+// // for loop 1 se 100
+// // for of loop 
+// // for in loop
+// // no=10
+// // cnt=0;
+// // while(cnt<=no){
+// //     // console.log(cnt);
+// //     cnt++;
+// // }
+
+// // console.log(cnt);
+
+
+// let cnt=5;
+
+// do{
+//     console.log(cnt);
+//     cnt++;
+// }while(false);
+
+
+// for(let i=0; i<=10; i++){
 //     console.log(i)
-//     // 0
-//     // 1
-//     // 2
-//     // 3
-//     // 4
-//     // 5
-//     // 6
 // }
 
-// console.log(i)
+// function in javascript
+// function declaretion 
+// fucntion definition 
+// function excution
 
-
-// function name(){
-//     var elem=10;
+// function funcName(){
+//     console.log("Hello Wolrd")
 // }
 
-// console.log(elem);
+// funcName()
 
-let sname="prashant";
-console.log(sname);
-// redefined
+// pass somthing return nothing
+// pass nothing return somthing
+// pass something return something
 
-// overwrite
-sname="aman"
-console.log(sname)
+// // pass nothing return nothing 
+// function sum(){
+//     console.log(10+20)
+// }
 
-// scoping
-{
-    let sno=50;
-}
-
-// console.log(sno)
-
-
-for(let i=0; i<=5; i++){
-    console.log(i);
-}
-
-// console.log(i);
+// sum();
+// pass nothing return nothing
+// the function which has no name 
+// let sum=function(a,b=20){
+//     console.log(a+b)
+// }
+// sum(10,50);
+// anonymous type 2
+let sum=(a,b)=> a+b;console.log(sum(10,20))
 
 
-const username="Admin";
-// username="admin"
+// console.log(sum(10,20))
 
-{
-    const  sum=10+20;
-    console.log(sum)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.txt
@@ -0,0 +1,4 @@
+a="09"
+echo "$a"
+echo "${a#0}"
+printf "%04d" "${a#0}"
